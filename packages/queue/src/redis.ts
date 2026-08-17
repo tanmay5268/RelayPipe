4f82a3ef4819b28a593796ecff8aa339e70f99ae
tanmay5268 <tanmay.mewati94@gmail.com>
2026-08-17 17:37:08 +0530

    update redis.ts

--- packages/queue/src/redis.ts
+++ packages/queue/src/redis.ts
@@ -4,9 +4,12 @@ import Redis from "ioredis";
 const redisUrl = process.env.UPSTASH_REDIS_URL;
 
 if (!redisUrl) {
-  throw new Error("UPSTASH_REDIS_URL is not set");
+  console.warn(
+    "[@repo/queue] UPSTASH_REDIS_URL is not set - queue operations will fail at runtime until it is provided.",
+  );
 }
 
-export const client = new Redis(redisUrl, {
+export const client = new Redis(redisUrl!, {
   maxRetriesPerRequest: null,
+  lazyConnect: true,
 });
